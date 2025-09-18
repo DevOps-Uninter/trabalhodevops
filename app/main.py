1bebe4ce63887c2b3ca3cb443ce5e83eefad5deb
Cleberton Silva <cleberton.silva@quipux.com.br>
2025-09-18 10:01:03 -0300

    fix: corrige nome da rota

--- app/main.py
+++ app/main.py
@@ -4,13 +4,13 @@
 from fastapi import FastAPI
 
 # Internal libraries
+from app.database import Base, engine
 from app.routers.pedidos import router as pedidos_router
 from app.routers.clientes import router as clientes_router
 from app.routers.relatorios import router as relatorios_router
 from app.routers.produtos import router as produtos_router
 from app.routers.pagamentos import router as pagamentos_router
-from app.routers.entregas import router as entregas_reouter
-from app.database import Base, engine
+from app.routers.entregas import router as entregas_router
 
 
 # Cria todas as tabelas no banco
@@ -41,4 +41,4 @@ app.include_router(clientes_router, prefix="/clientes", tags=["Clientes"])
 app.include_router(relatorios_router, prefix="/relatorios", tags=["Relatórios"])
 app.include_router(produtos_router)
 app.include_router(pagamentos_router)
-app.include_router(entregas_reouter)
+app.include_router(entregas_router)
